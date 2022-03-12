3b445222a0538039a73c9f50d62381df89536906
Hossein samiei <hossein@Hosseins-MacBook-Pro.local>
2022-03-12 21:47:14 +0330

    for handling products in store

---
+++ product.py
@@ -0,0 +1,17 @@
+class Product :
+
+    def __init__(self , name , price , number_of_remain):
+        self.name = name 
+        self.price = price 
+        self.number_of_remain = number_of_remain
+        self.info = {'name' : self.name , 'price' : self.price , "number_of_remain" : self.number_of_remain}
+
+
+
+
+    
+
+    
+
+
+    
